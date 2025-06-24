c81b0b0d3ce9c54622ed7053faa57255748636ea
ĐứC TâN <nguyenductandevtech@gmail.com>
2025-06-24 10:03:21 +0700

    basic content board

--- src/pages/Boards/BoardContent/index.jsx
+++ src/pages/Boards/BoardContent/index.jsx
@@ -1,16 +1,125 @@
-import Box from '@mui/material/Box'
+import { useState } from 'react'
+import { Typography, Menu, MenuItem, Box, Button, Divider, ListItemIcon, ListItemText, Tooltip } from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import ContentCut from '@mui/icons-material/ContentCut'
+import ContentCopy from '@mui/icons-material/ContentCopy'
+import Cloud from '@mui/icons-material/Cloud'
+import theme from '~/theme'
+import { AddCard, DeleteForever, DragHandle } from '@mui/icons-material'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
 
 function BoardContent() {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
     <Box sx={{
       backgroundColor: 'primary.main',
       width: '100%',
-      height: (theme) => theme.trello.contentBarHeight,
-      display: 'flex',
-      alignItems: 'center'
-
+      height: (theme) => theme.trello.boardContentHeight
     }}>
-      content box
+      <Box sx={{
+        minWidth: '300px',
+        maxWidth: '300px',
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0'),
+        ml: 2,
+        borderRadius: '6px'
+      }}>
+        <Box sx={{
+          height: (theme) => theme.trello.columnHeaderHeight,
+          p: 2,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between'
+        }}>
+          <Typography>Column title</Typography>
+          <Tooltip title='more options'>
+            <ExpandMoreIcon
+              id = 'basic-button-list'
+              sx={{color:'text.primary', cursor:'pointer'}}
+              aria-haspopup='true'
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-expanded={open ? 'true' : undefined}
+              onClick = {handleClick}
+
+            />
+          </Tooltip>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button-workspaces'
+            }}
+          >
+            <MenuItem>
+              <ListItemIcon><ContentCut fontSize="small" /></ListItemIcon>
+              <ListItemText>Cut</ListItemText>
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon><ContentCopy fontSize="small" /></ListItemIcon>
+              <ListItemText>Copy</ListItemText>
+            </MenuItem>
+            <Divider />
+            <MenuItem>
+              <ListItemIcon><DeleteForever fontSize="small" /></ListItemIcon>
+              <ListItemText>Delete Clipboard</ListItemText>
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon><Cloud fontSize="small" /></ListItemIcon>
+              <ListItemText>Web Clipboard</ListItemText>
+            </MenuItem>
+          </Menu>
+        </Box>
+
+        <Box>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              sx={{ height: 140 }}
+              image="https://images.pexels.com/photos/56010/dragonfly-insect-animal-wing-56010.jpeg"
+              title="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                Lizards are a widespread group of squamate reptiles, with over 6,000
+                species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Box>
+
+        <Box sx={{
+          height: (theme) => theme.trello.columnFooterHeight,
+          p: 2,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between'
+        }}>
+          <Button startIcon={<AddCard/>}>Add new card</Button>
+          <Tooltip title='Drag the column'>
+            <DragHandle sx={{cursor:'pointer'}}/>
+          </Tooltip>
+        </Box>
+      </Box>
     </Box>
   )
 }
